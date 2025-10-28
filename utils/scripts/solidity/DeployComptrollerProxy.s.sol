d7ebb927141da2e19823f242fa57586628d83cbd
Shubham Yadav <shubhamy2015@gmail.com>
2025-10-28 12:17:18 +0000

    chore: sync utils package (#4)
    * chore: sync utils package
    
    * chore: update package.json

--- utils/scripts/solidity/DeployComptrollerProxy.s.sol
+++ utils/scripts/solidity/DeployComptrollerProxy.s.sol
@@ -1,40 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.22;
 
-import { Options, Upgrades } from "@openzeppelin/foundry-upgrades/src/Upgrades.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { SablierComptroller } from "src/SablierComptroller.sol";
-import { BaseScript } from "src/tests/BaseScript.sol";
+import { ProxyHelpers } from "./ProxyHelpers.sol";
 
 /// @notice Deploys a new proxy and the Sablier Comptroller.
-/// @dev The deployed Sablier Comptroller is set as the implementation of the proxy. See
-/// https://docs.openzeppelin.com/upgrades-plugins/foundry-upgrades#usage for more details.
-contract DeployComptrollerProxy is BaseScript {
+/// @dev The deployed Sablier Comptroller is set as the implementation of the proxy.
+contract DeployComptrollerProxy is ProxyHelpers {
     function run() public broadcast returns (address proxy, address implementation) {
-        // Declare the constructor parameters of the implementation contract.
-        Options memory opts;
-        opts.constructorData = abi.encode(getAdmin());
+        // Run upgrade safety checks.
+        _runUpgradeSafetyChecks();
 
-        // Disable the constructor check for the implementation contract. See
-        // https://docs.openzeppelin.com/upgrades-plugins/faq#how-can-i-disable-checks.
-        opts.unsafeAllow = "constructor";
+        // Deploy implementation contract with default admin as its initial admin.
+        address impl = address(new SablierComptroller({ initialAdmin: DEFAULT_SABLIER_ADMIN }));
 
-        // Declare the initializer data for the proxy.
-        bytes memory initializerData = abi.encodeCall(
-            SablierComptroller.initialize,
-            (getAdmin(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getChainlinkOracle())
-        );
+        // Deploy proxy without initialization.
+        proxy = address(new ERC1967Proxy({ implementation: impl, _data: "" }));
 
-        // Perform the following steps:
-        // 1. Deploys the proxy.
-        // 2. Deploys the Comptroller.
-        // 3. Sets the implementation of the proxy to the address of the deployed Comptroller.
-        // 4. Initializes the state variables in the proxy contract.
-        proxy = Upgrades.deployUUPSProxy({
-            contractName: "SablierComptroller.sol:SablierComptroller",
-            initializerData: initializerData,
-            opts: opts
-        });
+        // Initialize the proxy by populating its initial states.
+        _populateInitialStates(proxy);
 
-        implementation = Upgrades.getImplementationAddress(proxy);
+        // Get implementation address by reading the implementation storage slot of the proxy.
+        implementation = _getImplementation(proxy);
+
+        // Verify the initial states of the proxy contract.
+        _verifyInitialStates(proxy);
     }
 }
