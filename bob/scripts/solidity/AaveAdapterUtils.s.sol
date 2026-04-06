2c049361bca7e0c01807e16784af4a84b73f7e21
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2026-04-06 13:37:20 +0300

    chore(bob): add aave adapter script

---
+++ bob/scripts/solidity/AaveAdapterUtils.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.22 <0.9.0;
+
+import { UD60x18 } from "@prb/math/src/UD60x18.sol";
+import { ChainId } from "@sablier/evm-utils/src/tests/ChainId.sol";
+
+/// @notice Aave Adapter utility functions for deploy scripts.
+abstract contract AaveAdapterUtils {
+    UD60x18 internal constant INITIAL_YIELD_FEE = UD60x18.wrap(0.1e18); // 10%
+
+    /// @dev Returns the Aave V3 PoolAddressesProvider for the current chain.
+    /// See https://docs.aave.com/developers/deployed-contracts/v3-mainnet
+    function getAavePoolAddressesProvider() internal view returns (address aavePoolAddressesProvider) {
+        if (block.chainid == ChainId.ETHEREUM) {
+            aavePoolAddressesProvider = 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e;
+        } else if (block.chainid == ChainId.SEPOLIA) {
+            aavePoolAddressesProvider = 0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A;
+        }
+    }
+}
