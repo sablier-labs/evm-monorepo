19ad1f2c9f53f1fcf5709ee8e57cac84ae7306ba
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2026-04-21 16:19:33 +0300

    test(bob-fuzz): add missing tests

--- bob/tests/bob/integration/fuzz/bob/enter.t.sol
+++ bob/tests/bob/integration/fuzz/bob/enter.t.sol
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.22 <0.9.0;
 
-import { ud } from "@prb/math/src/UD60x18.sol";
-
 import { IBobVaultShare } from "src/interfaces/IBobVaultShare.sol";
 import { ISablierBob } from "src/interfaces/ISablierBob.sol";
-import { ISablierBobAdapter } from "src/interfaces/ISablierBobAdapter.sol";
 
 import { Integration_Test } from "../../Integration.t.sol";
 
@@ -72,17 +69,7 @@ contract Enter_Integration_Fuzz_Test is Integration_Test {
         IBobVaultShare shareTokenForVaultWithAdapter = bob.getShareToken(vaultIds.vaultWithAdapter);
         uint256 shareBalanceBefore = shareTokenForVaultWithAdapter.balanceOf(users.newDepositor);
 
-        uint128 expectedWstETH = ud(amount).mul(WSTETH_WETH_EXCHANGE_RATE).intoUint128();
-
-        // It should emit {Stake} and {Enter} events.
-        vm.expectEmit({ emitter: address(adapter) });
-        emit ISablierBobAdapter.Stake({
-            vaultId: vaultIds.vaultWithAdapter,
-            user: users.newDepositor,
-            depositAmount: amount,
-            wrappedStakedAmount: expectedWstETH
-        });
-
+        // It should emit an {Enter} event.
         vm.expectEmit({ emitter: address(bob) });
         emit ISablierBob.Enter({
             vaultId: vaultIds.vaultWithAdapter,
@@ -99,9 +86,5 @@ contract Enter_Integration_Fuzz_Test is Integration_Test {
         // It should mint share tokens 1:1 with the deposit amount.
         uint256 actualSharesMinted = shareTokenForVaultWithAdapter.balanceOf(users.newDepositor) - shareBalanceBefore;
         assertEq(actualSharesMinted, amount, "sharesMinted");
-
-        // It should stake via the adapter and track wstETH.
-        uint256 actualWstETH = adapter.getYieldBearingTokenBalanceFor(vaultIds.vaultWithAdapter, users.newDepositor);
-        assertEq(actualWstETH, expectedWstETH, "userWstETH");
     }
 }
